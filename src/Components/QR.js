fb37a6fd006821b4578a946020425ad8e20f1e8b
Hita Juneja <hitajuneja@gmail.com>
2022-04-14 14:09:20 +0530

    qr

--- src/Components/QR.js
+++ src/Components/QR.js
@@ -3,8 +3,11 @@ import img from '../media/img/qrcode.png'
 
 export default function QR() {
   return (
-    <div id='qr'>
-        <img src={img} alt="alt" className='h-48 w-48 place-content-center'/>
+    <div id='qr' className='justify-center align-center'><br/>
+      <h2>Scan QR Code to transfer CEROs!</h2><br/>
+      <div>
+        <img src={img} alt="alt" className='h-48 w-48'/>
+      </div>
     </div>
   )
 }
